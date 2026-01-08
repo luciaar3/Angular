00c8b10fdab94ff8c691b2aa5b3e795afdeaa26d
lucia <luciaandreu@gmail.com>
2026-01-08 17:54:03 +0100

    UD3

---
+++ ejUD3/src/app/user-profile/user-profile.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  imports: [],
+  templateUrl: './user-profile.html',
+  styleUrl: './user-profile.css',
+})
+export class UserProfile {
+  profileImage = 'avatar.avif';
+  isPremium = true;
+}
